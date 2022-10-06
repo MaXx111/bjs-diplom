65e2a25d2547ee2cc6c94e319c1a83f5fb1fc4ed
Sidorov Maksim <maxx11167@gmail.com>
2022-10-06 17:16:53 +0300

    Complete task, needs check

---
+++ public/homePage.js
@@ -0,0 +1,126 @@
+"use strict";
+
+const bestLogoutButton = new LogoutButton ();
+
+bestLogoutButton.action = f => {
+    ApiConnector.logout( logOut => {
+        if (logOut.success) {
+            location.reload();
+        } else {
+            console.log('ты от нас не убежишь :з');
+        }
+    })
+}
+
+ApiConnector.current(data => {
+    if (data.success){
+        ProfileWidget.showProfile(data.data);
+    }
+});
+
+
+const bestRatesBoard = new RatesBoard();
+
+function makeBoard() {
+    let timerId = null;
+    if (!timerId) {
+        ApiConnector.getStocks(data => {
+            if (data.success){
+                bestRatesBoard.clearTable();
+                bestRatesBoard.fillTable(data.data) 
+            } 
+        });  
+    } 
+
+    setInterval(() => ApiConnector.getStocks(data => {
+        if (data.success){
+            bestRatesBoard.clearTable();
+            bestRatesBoard.fillTable(data.data) 
+        } 
+    }), 60000);
+}
+
+makeBoard();
+
+
+const moneyManager = new MoneyManager();
+
+moneyManager.addMoneyCallback = function addMoney({ currency, amount }) {
+    ApiConnector.addMoney(({ currency, amount }), some => {
+        if (some.success) {
+            ProfileWidget.showProfile(some.data);
+            some.message = 'успех'
+        } else {
+            some.message = 'провал'
+        }
+        moneyManager.setMessage(some.success, some.message);
+    });
+
+};
+
+moneyManager.conversionMoneyCallback = function convertMoney(data) {
+    ApiConnector.convertMoney(data, some => {
+        if (some.success) {
+            ProfileWidget.showProfile(some.data);
+            some.message = 'успех'
+        } else {
+            some.message = 'провал'
+        }
+        moneyManager.setMessage(some.success, some.message);
+    }); 
+};
+
+moneyManager.sendMoneyCallback = function sendMoney(data) {
+    ApiConnector.transferMoney(data, some => {
+        if (some.success) {
+            ProfileWidget.showProfile(some.data);
+            some.message = 'успех'
+        } else {
+            some.message = 'провал'
+        }
+        moneyManager.setMessage(some.success, some.message);
+    });
+}
+
+
+const favoritesWidget = new FavoritesWidget();
+
+ApiConnector.getFavorites(some => {
+    if (some.success){
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(some.data);
+        moneyManager.updateUsersList(some.data);
+    }
+});
+
+favoritesWidget.addUserCallback = function addUser(data) {
+    ApiConnector.addUserToFavorites(data, some => {
+        if (some.success) {
+            some.message = 'успех';
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(some.data);
+            moneyManager.updateUsersList(some.data);
+            favoritesWidget.setMessage(some.success, some.message)
+        } else {
+            some.message = 'провал';
+            favoritesWidget.setMessage(some.success, some.message)
+        }
+    });
+}
+
+favoritesWidget.removeUserCallback = function removeUser(data) {
+    ApiConnector.removeUserFromFavorites(data, some => {
+        if (some.success) {
+                some.message = 'успех';
+                favoritesWidget.clearTable();
+                favoritesWidget.fillTable(some.data);
+                moneyManager.updateUsersList(some.data);
+                favoritesWidget.setMessage(some.success, some.message)
+            } else {
+                some.message = 'провал';
+                favoritesWidget.setMessage(some.success, some.message)
+            }
+    });
+}
+
+
--- public/loginPage.js
+++ public/loginPage.js
@@ -1,8 +1,29 @@
 "use strict";
 
-class UserForm {
-    constructor () {
+const bestUserForm = new UserForm ();
 
-    }
+bestUserForm.loginFormCallback = data => {
+    let login = data.login;
+    let password = data.password;
 
+    ApiConnector.login({login, password}, data => {
+        if (data.success) {
+            location.reload();
+        } else {
+            bestUserForm.setLoginErrorMessage(data.error);
+        }
+    })
+}
+
+bestUserForm.registerFormCallback = data => {
+    let login = data.login;
+    let password = data.password;
+
+    ApiConnector.register({login, password}, data => {
+        if (login && password) {
+            location.reload();
+        } else {
+            bestUserForm.setRegisterErrorMessage('Все херня, давай по новой');
+        }
+    })
 }
